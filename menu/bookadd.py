fe60174e17329ee526e2b76427186a81887ac347
jinilcs <jinilcs@gmail.com>
2018-01-29 21:25:29 -0500

    Adding files

---
+++ menu/bookadd.py
@@ -0,0 +1,3 @@
+import  menu.displaymenu
+
+displaymenu.getoption()
---
+++ menu/displaymenu.py
@@ -0,0 +1,14 @@
+def getoption():
+
+    print('Online book store !!!')
+
+    choice = input('''
+    
+    1. Add a book
+    2. Read a book
+    3. Delete a book
+    
+    Please select 1 option: 
+    ''')
+
+    return choice
